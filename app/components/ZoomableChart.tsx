0aa502f4e2c4aab0178f9dd227521c6ff05c2508
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-26 12:01:05 +0530

    size fixed

--- app/components/ZoomableChart.tsx
+++ app/components/ZoomableChart.tsx
@@ -5,121 +5,126 @@ import * as d3 from "d3";
 const data = json;
 
 export const ZoomableChart = () => {
-  const svgRef = useRef(null);
+	const svgRef = useRef(null);
 
-  useEffect(() => {
-    // Specify the chart╬ô├ç├ûs dimensions.
-    const width = 928;
-    const height = 1200;
+	useEffect(() => {
+		if (!svgRef.current) return;
 
-    // Create the color scale.
-    const color = d3.scaleOrdinal(
-      d3.quantize(d3.interpolateRainbow, data.children.length + 1),
-    );
+		d3.select(svgRef.current).selectAll("svg").remove();
+		// Specify the chart╬ô├ç├ûs dimensions.
+		const width = 1400;
+		const height = 700;
 
-    // Compute the layout.
-    const hierarchy = d3
-      .hierarchy(data)
-      .sum((d: any) => d.value)
-      .sort((a, b) => {
-        const aValue = a.value || 0;
-        const bValue = b.value || 0;
-        return b.height - a.height || bValue - aValue;
-      });
-    const root = d3
-      .partition()
-      .size([height, ((hierarchy.height + 1) * width) / 3])(hierarchy as any);
+		// Create the color scale.
+		const color = d3.scaleOrdinal(
+			d3.quantize(d3.interpolateRainbow, data.children.length + 1),
+		);
 
-    // Append cells.
-    const svg = d3.select(svgRef.current);
+		// Compute the layout.
+		const hierarchy = d3
+			.hierarchy(data)
+			.sum((d: any) => d.value)
+			.sort((a, b) => {
+				const aValue = a.value || 0;
+				const bValue = b.value || 0;
+				return b.height - a.height || bValue - aValue;
+			});
+		const root = d3
+			.partition()
+			.size([height, ((hierarchy.height + 1) * width) / 3])(hierarchy as any);
 
-    const cell = svg
-      .selectAll("g")
-      .data(root.descendants())
-      .join("g")
-      .attr("transform", (d) => `translate(${d.y0},${d.x0})`)
-      .on("click", clicked);
-    const rect = cell
-      .append("rect")
-      .attr("width", (d) => d.y1 - d.y0 - 1)
-      .attr("height", (d) => rectHeight(d))
-      .attr("fill-opacity", 0.6)
-      .attr("fill", (d) => {
-        if (!d.depth) return "#ccc";
-        while (d.depth > 1) d = d.parent as any;
-        return color((d.data as any).name);
-      })
-      .style("cursor", "pointer");
+		// Append cells.
+		const svg = d3
+			.select(svgRef.current)
+			.append("svg")
+			.attr("viewBox", [0, 0, width, height])
+			.attr("width", width)
+			.attr("height", height)
+			.attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
 
-    const text = cell
-      .append("text")
-      .style("user-select", "none")
-      .attr("pointer-events", "none")
-      .attr("x", 4)
-      .attr("y", 13)
-      .attr("fill-opacity", (d) => +labelVisible(d));
+		const cell = svg
+			.selectAll("g")
+			.data(root.descendants())
+			.join("g")
+			.attr("transform", (d) => `translate(${d.y0},${d.x0})`)
+			.on("click", clicked);
+		const rect = cell
+			.append("rect")
+			.attr("width", (d) => d.y1 - d.y0 - 1)
+			.attr("height", (d) => rectHeight(d))
+			.attr("fill-opacity", 0.6)
+			.attr("fill", (d) => {
+				if (!d.depth) return "#ccc";
+				while (d.depth > 1) d = d.parent as any;
+				return color((d.data as any).name);
+			})
+			.style("cursor", "pointer");
 
-    text.append("tspan").text((d) => (d.data as any).name);
+		const text = cell
+			.append("text")
+			.style("user-select", "none")
+			.attr("pointer-events", "none")
+			.attr("x", 4)
+			.attr("y", 13)
+			.attr("fill-opacity", (d) => +labelVisible(d));
 
-    const format = d3.format(",d");
-    const tspan = text
-      .append("tspan")
-      .attr("fill-opacity", (d) => +labelVisible(d) * 0.7)
-      .text((d) => ` ${format(d.value as number)}`);
+		text.append("tspan").text((d) => (d.data as any).name);
 
-    cell.append("title").text(
-      (d) =>
-        `${d
-          .ancestors()
-          .map((d) => (d.data as any).name)
-          .reverse()
-          .join("/")}\n${format(d.value as number)}`,
-    );
+		const format = d3.format(",d");
+		const tspan = text
+			.append("tspan")
+			.attr("fill-opacity", (d) => +labelVisible(d) * 0.7)
+			.text((d) => ` ${format(d.value as number)}`);
 
-    let focus: any = root;
-    function clicked(event: MouseEvent, p: any) {
-      if (!p.parent) return;
-      focus = focus === p ? (p = p.parent || root) : p;
+		cell.append("title").text(
+			(d) =>
+				`${d
+					.ancestors()
+					.map((d) => (d.data as any).name)
+					.reverse()
+					.join("/")}\n${format(d.value as number)}`,
+		);
 
-      root.each(
-        (d: any) =>
-          (d.target = {
-            x0: ((d.x0 - p.x0) / (p.x1 - p.x0)) * height,
-            x1: ((d.x1 - p.x0) / (p.x1 - p.x0)) * height,
-            y0: d.y0 - p.y0,
-            y1: d.y1 - p.y0,
-          }),
-      );
+		let focus: any = root;
+		function clicked(event: MouseEvent, p: any) {
+			if (!p.parent) return;
+			focus = focus === p ? (p = p.parent || root) : p;
 
-      const t = cell
-        .transition()
-        .duration(750)
-        .attr(
-          "transform",
-          (d: any) => `translate(${d.target.y0},${d.target.x0})`,
-        );
+			root.each(
+				(d: any) =>
+				(d.target = {
+					x0: ((d.x0 - p.x0) / (p.x1 - p.x0)) * height,
+					x1: ((d.x1 - p.x0) / (p.x1 - p.x0)) * height,
+					y0: d.y0 - p.y0,
+					y1: d.y1 - p.y0,
+				}),
+			);
 
-      rect.transition(t).attr("height", (d: any) => rectHeight(d.target));
-      text
-        .transition(t)
-        .attr("fill-opacity", (d: any) => +labelVisible(d.target));
-      tspan
-        .transition(t)
-        .attr("fill-opacity", (d: any) => +labelVisible(d.target) * 0.7);
-    }
+			const t = cell
+				.transition()
+				.duration(750)
+				.attr(
+					"transform",
+					(d: any) => `translate(${d.target.y0},${d.target.x0})`,
+				);
 
-    function rectHeight(d: any) {
-      return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
-    }
+			rect.transition(t).attr("height", (d: any) => rectHeight(d.target));
+			text
+				.transition(t)
+				.attr("fill-opacity", (d: any) => +labelVisible(d.target));
+			tspan
+				.transition(t)
+				.attr("fill-opacity", (d: any) => +labelVisible(d.target) * 0.7);
+		}
 
-    function labelVisible(d: any) {
-      return d.y1 <= width && d.y0 >= 0 && d.x1 - d.x0 > 16;
-    }
-  }, []);
+		function rectHeight(d: any) {
+			return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
+		}
 
-  return (
-    <div className="w-full h-full">
-      <svg ref={svgRef} width="100%" height="100%"></svg>
-    </div>
-  );
+		function labelVisible(d: any) {
+			return d.y1 <= width && d.y0 >= 0 && d.x1 - d.x0 > 16;
+		}
+	}, []);
+
+	return <div className="w-full h-full" ref={svgRef}></div>;
 };
